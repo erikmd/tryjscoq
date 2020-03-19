6da25647160d7b6b163ebc5d183aa3449edb97c3
Erik Martin-Dorel <erik@martin-dorel.org>
2020-03-19 05:33:15 +0100

    Add functional/proving exercises

--- ivvq/preuves.v
+++ ivvq/preuves.v
@@ -1,6 +1,70 @@
 Require Import List. Import ListNotations.
 Set Implicit Arguments.
 
+(** * Écriture de petits programmes fonctionnels *)
+
+(** Ecrire la fonction mult2 retournant le double de son argument. *)
+Definition mult2 := 0.
+
+(** Quel est le type de l'expression
+    [ fun (T:Type) (f:T -> T) x => f (f x) ] ? *)
+
+(** Quel est le type de l'expression [ fun (T:Type) => T -> T ] ? *)
+
+(** Quel est le type de l'expression [ fun (T:Type) (x:T) => x=x ] ? *)
+
+(** Ecrire une fonction de type
+    [ forall (a : Type) (b : Type), a -> b -> a ] *)
+Definition faba := 0.
+
+(** Ecrire une fonction de type
+    [ forall a b c : Type, (a -> b -> c) -> b -> a -> c ] *)
+Definition f' := 0.
+
+(** Exercice 10 *)
+(* Choix 1 *)
+(** Écrire la fonction [inserer] qui, étant donné un entier et une
+liste déjà triée, construit la liste où l’entier est inséré en bonne
+position dans la liste.
+
+En déduire une fonction [trierInsertion] qui trie une liste
+d’entiers dans l’ordre croissant.
+ *)
+
+(* Choix 2 - Tri fusion *)
+(** Écrire la fonction [partage] qui partage une liste en deux listes
+l1 et l2 telles que les tailles de l1 et l2 ne diffèrent que d’un au
+maximum.
+
+Écrire une fonction [merge] qui prend en argument deux listes
+ordonnées d’entiers et renvoie une liste ordonnée.
+
+En déduire une fonction [trierFusion] qui trie une liste d’entiers
+dans l’ordre croissant. *)
+
+(* Choix 3 - Hanoï *)
+(** Le problème des tours de Hanoï est un
+  jeu de réflexion imaginé par le mathématicien français Édouard
+  Lucas. Le but du jeu est de déplacer des disques de diamètres
+  différents d'une tour de départ (Tour 1) à une tour d'arrivée (Tour
+  3) en passant par une tour intermédiaire (Tour 2).
+
+  Les règles de jeu sont les suivantes :
+  - on ne peut déplacer plus d'un disque à la fois
+  - on ne peut placer un disque que sur un autre disque plus grand
+    que lui ou sur un emplacement vide.
+  
+  Ecrire la fonction [hanoi] qui étant donné le nombre [n] de
+  disques retourne la liste des mouvements définis par des couples
+  (numéro de la tour de départ, numéro de la tour d’arrivée).
+
+  Indication :
+  [ Check hanoi :
+      forall (T: Type) (src dst tmp : T) (n : nat), list (T * T). ]
+  [ Eval compute in hanoi nat 1 3 2 2.
+    (* [(1,2);(1,3);(2,3)] *) ]
+ *)
+
 (** * Preuve assistée par Coq *)
 
 (** Premier exemple de théorème *)
@@ -121,7 +185,14 @@ Theorem rev_len :
 Proof.
 Admitted.
 
-(* Exercices pour les plus rapides :
- - Prouver quelle est la longueur des listes renvoyées par la fonction hanoi.
- - Plus difficile : spécifier & prouver que le tri par insertion est correct.
+(** * Exercice final d'application du cours *)
+
+(** Prouver quelle est la longueur des listes renvoyées par la
+fonction [hanoi] ; s'aider de la bibliothèque standard ! :
+
+- https://coq.github.io/doc/v8.11/stdlib/
+- https://coq.github.io/doc/v8.11/refman/proof-engine/vernacular-commands.html?highlight=search#coq:cmd.search
  *)
+
+(** Plus difficile : spécifier & prouver que le tri par insertion est
+correct. *)
